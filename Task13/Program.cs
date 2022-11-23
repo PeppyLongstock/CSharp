d3ee8a005b2eaa207852e5560c5ca07b5efc409a
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-11-23 21:01:03 +0300

    all the tasks

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного
+ числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+Console.Write("Enter a Number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string SecondNumber = Convert.ToString(Number);
+Console.WriteLine("The second digit of this number: "+SecondNumber[1]);
---
+++ Task15/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber)
+{
+  if (dayNumber == 6 || dayNumber == 7)
+  {
+  Console.WriteLine("this day is a day off");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("7 days in a week!");
+  }
+  else Console.WriteLine("this day is working day");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
