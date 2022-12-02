dcfbcccdad0a8f5302a265fc446bf205cd16332f
PeppyLongstock <sharlottakorde@gmail.com>
2022-12-02 09:42:41 +0300

    HomeWork7 added

---
+++ HomeWork7/Task50/Program.cs
@@ -0,0 +1,107 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+int[,] GetRandomArray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array;
+}
+void print2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] GetArrayConversion(int[,] array)
+{
+
+    int sizeNewArray = array.GetLength(0) * array.GetLength(1);
+    int[] newArray = new int[sizeNewArray];
+    int index = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArray[index] = array[i, j];
+            index++;
+        }
+    }
+    return newArray;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+void OutputUserElement(int[] array)
+{
+    Console.Write("Enter the element position: ");
+    int userElement = int.Parse(Console.ReadLine());
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (userElement == i)
+        {
+            Console.WriteLine($" The element with the index is {userElement} = {array[i]}");
+        }
+    }
+
+    if (userElement > array.Length)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("No such number in the array");
+        Console.ResetColor();
+        Console.WriteLine();
+    }
+    if (userElement < 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("No such number in the array");
+        Console.ResetColor();
+        Console.WriteLine();
+    }
+}
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("2Darray:");
+int[,] array1 = GetRandomArray(3, 4, 10);
+print2dArray(array1);
+Console.ResetColor();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Converted 1d Array:");
+int[] array2 = GetArrayConversion(array1);
+PrintArray(array2);
+Console.ResetColor();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Yellow;
+OutputUserElement(array2);
+Console.ResetColor();
---
+++ HomeWork7/Task52/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+*/
+int[,] generate2DArray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    {
+        for (int i = 0; i < lengthRow; i++)
+        {
+            for (int j = 0; j < lengthCol; j++)
+            {
+                array[i, j] = new Random().Next(-deviation, deviation + 1);
+            }
+        }
+    }
+
+    return array;
+}
+
+void printColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+
+void print2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void AverageArray(int[,] array)
+{
+    double average = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double summ = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            summ = summ + array[i, j];
+        }
+        average = summ / array.GetLength(0);
+        Console.WriteLine($"The average of the column {j} is {average}");
+    }
+}
+
+
+int[,] array = generate2DArray(4, 4, 10);
+print2dArray(array);
+Console.WriteLine();
+AverageArray(array);
